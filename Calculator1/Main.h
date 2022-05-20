c36d6b9a0208699afcef925f392d45523ea449ce
RubenLozano27 <94849547+RubenLozano27@users.noreply.github.com>
2022-05-20 18:19:02 -0400

    finsihed

--- Calculator1/Main.h
+++ Calculator1/Main.h
@@ -1,6 +1,6 @@
 #pragma once
 #include"wx/wx.h"
-
+#include "ButtonFactory.h"
 class Main :public wxFrame
 {
 public:
@@ -8,8 +8,10 @@ public:
 	~Main();
 
 public:
+	ButtonFactory buttonbuilder;
 	wxButton* btn[21];
 	bool decimalClick = false;
+	wxString titles[21] = { "0","1","2","3","4","5","6","7","8","9","Bin","Hex","Dex","Mod","+","-","*","/","=","C","."};
 	wxTextCtrl* calcDisplay;
 	void OnButtonClicked(wxCommandEvent& evt);
 
